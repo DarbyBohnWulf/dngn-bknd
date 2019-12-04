06a9ce5504c4de7af392d6954080c5c6679d54c2
Mark Ritchie <mwritchie2@gmail.com>
2019-12-04 16:10:42 -0600

    enabled password hashing on user create

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,9 @@
 import { composeWithMongoose } from 'graphql-compose-mongoose/node8';
 import { schemaComposer } from 'graphql-compose';
+import bcrypt from 'bcrypt';
 import User from '../models/user';
+
+const saltRounds = 10; // for bcrypt hashing
 
 // here is where GQL will get its model
 const UserTC = composeWithMongoose(User);
@@ -8,22 +11,11 @@ const UserTC = composeWithMongoose(User);
 // this wrapper will handle password hashing for new users
 UserTC.wrapResolverResolve('createOne', next => async rp => {
   rp.beforeRecordMutate = async (doc, resolveParams) => {
-    
+    doc.password = await bcrypt.hash(doc.password, saltRounds)
     return doc
   }
   return next(rp);
 });
-
-// UserTC.addResolver({
-//   name: 'createOne',
-//   args: {username: 'String', email: 'String!', password: 'String!'},
-//   type: UserTC,
-//   resolve: async ({source, args}) => {
-//     const res = await fetch('http://localhost:3080/graphql');
-//     const data = await res.json();
-
-//   }
-// })
 
 // here is where we compose all the resolvers
 schemaComposer.Query.addFields({
