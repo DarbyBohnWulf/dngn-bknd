eb46ec6ea8d8d97fc08155174d1236a83d1831cd
Mark Ritchie <mwritchie2@gmail.com>
2019-12-03 21:36:36 -0600

    added polyfills to allow es6+ features

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
-import express from 'express';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import express, { Request, Response } from 'express';
 
 const PORT = 3080;
 const app = express();
+
+app.get('/', async (req,res, next) => {
+  try {
+    res.status(227).send('Gottem');
+  } catch (err) {
+    console.error("Couldn't get /\n", err);
+  }
+});
 
 app.listen(PORT, () => {
   console.log("App listening on port " + PORT);
